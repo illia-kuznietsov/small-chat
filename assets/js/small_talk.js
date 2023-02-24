955589515bedabe8ac468cc19e836634ec5374f8
Illia Kuznietsov <illiakuznietsov@lits-admins-MacBook-Pro.local>
2023-02-24 18:13:40 +0200

    added like buttons with logic, that is not yet working

--- assets/js/small_talk.js
+++ assets/js/small_talk.js
@@ -15,12 +15,26 @@ let SmallTalk = {
             document.getElementById('user-msg').value = ''
         })
 
+
         channel.on('shout', payload => {
             let chatBox = document.querySelector('#chat-box')
             let msgBlock = document.createElement('p')
 
             msgBlock.insertAdjacentHTML('beforeend', `${payload.nickname} : ${payload.message}`)
             chatBox.appendChild(msgBlock)
+        })
+
+        let like_buttons = document.getElementsByClassName('like');
+        for (b of like_buttons){
+             b.addEventListener('onclick', function(e){
+                 channel.push('like', {id: e.id})
+             });
+        }
+
+        channel.on('like', payload => {
+            let button = document.getElementById(payload.id)
+            button.setAttribute(title, payload.likes)
+            button.value = payload.likes.length
         })
     }
 }
