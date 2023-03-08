3d7004a92b3f577e22185999700ad9837ee20539
Illia Kuznietsov <illiakuznietsov@lits-admins-MacBook-Pro.local>
2023-03-08 17:57:32 +0200

    3 out of 3 new filters now work, refactoring and testing await

--- lib/chat_web/live/filtration.ex
+++ lib/chat_web/live/filtration.ex
@@ -30,16 +30,33 @@ defmodule ChatWeb.Filtration do
     do: messages
 
   def not_liked_filter(messages, "on"),
-    do: messages
+    do:
+      Enum.filter(messages, fn message ->
+        message.likes == [] and
+          Enum.all?(messages, fn msg ->
+            message.username not in msg.likes
+          end)
+      end)
 
   def not_liked_filter(messages, "off"),
     do: messages
 
-  def minority_filter(messages, "on"),
-    do: messages
+  def minority_filter(messages, "on") do
+    total_like_count_global = count_total_likes(messages)
+    messages
+    |> Enum.sort_by(&length(&1.likes), :desc)
+    |> Enum.reduce_while([], fn message, acc ->
+      if length(message.likes) + count_total_likes(acc) < total_like_count_global * 0.8,
+        do: {:cont, [message | acc]},
+        else: {:halt, acc}
+    end)
+  end
 
   def minority_filter(messages, "off"),
     do: messages
+
+  def count_total_likes(messages),
+    do: Enum.reduce(messages, 0, fn message, acc -> acc + length(message.likes) end)
 
   defp filtrate_on_text(text) do
     case text do
