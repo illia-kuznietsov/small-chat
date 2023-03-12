64c3d46bf4d38c97f2ac5bb00397fbe26cc65194
Illia Kuznietsov <illiakuznietsov@lits-admins-MacBook-Pro.local>
2023-03-12 17:34:08 +0200

    tweaked tests

--- test/chat_web/live/chat_live_test.exs
+++ test/chat_web/live/chat_live_test.exs
@@ -7,19 +7,19 @@ defmodule ChatWeb.ChatLiveTest do
     {:ok, _view, html} = conn |> live
 
     assert html =~
-             "<form id=\"message_form\" phx-submit=\"send\"><input type=\"text\" placeholder=\"Your message\" name=\"message\"/><button>Send</button></form>"
+             "<form id=\"message_form\" phx-submit=\"send\" class=\"post-msg\"><input type=\"text\" placeholder=\"Your message\" name=\"message\"/><button>&amp;#x21E8 send &amp;#x21E6</button></form>"
   end
 
   test "another mount test", %{conn: conn} do
     conn = get(conn, "/chat")
-    assert html_response(conn, 200) =~ "<h1>Welcome to the Chat, #{conn.assigns.username}!</h1>"
+    assert html_response(conn, 200) =~ "<h1>Welcome to the Chat, \n #{conn.assigns.username}!</h1>"
   end
 
   test "renders ok" do
-    assigns = %{username: "test", messages: [], filter: "", checked: false}
+    assigns = %{username: "test", messages: [], filter: "", checked: false, profile: "", mini: ""}
 
     assert render_component(&ChatWeb.ChatLive.render/1, assigns) =~
-             "<h1>Welcome to the Chat, test!</h1>"
+             "<h1>Welcome to the Chat, \n test!</h1>"
   end
 
   test "chat box" do
